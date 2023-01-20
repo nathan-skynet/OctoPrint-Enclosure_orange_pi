7f9134080751c29146d3b8364e6b9eca25b667d5
sk7n4k3d <nathan_skynet@tutanota.com>
2023-01-20 20:24:02 +0100

    Update __init__.py

--- octoprint_enclosure/__init__.py
+++ octoprint_enclosure/__init__.py
@@ -5,7 +5,10 @@ from octoprint.util import RepeatedTimer
 from subprocess import Popen, PIPE
 from .ledstrip import LEDStrip
 import octoprint.plugin
-import RPi.GPIO as GPIO
+import orangepi.zero2
+from OPi import GPIO
+GPIO.setmode(orangepi.zero2.BOARD) 
+
 from flask import jsonify, request, make_response, Response
 from octoprint.server.util.flask import restricted_access
 from werkzeug.exceptions import BadRequest
